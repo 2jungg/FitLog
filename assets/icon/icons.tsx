3193539c1440f8e354f1b0c5934244912e8e9a10
jungg <jungkwen20@gmail.com>
2025-07-05 15:34:43 +0900

    try to use svg icons v1

---
+++ assets/icon/icons.tsx
@@ -0,0 +1,21 @@
+import DI_BLACK from './diet_icon_black.svg';
+import DI_COLOR from './diet_icon_color.svg';
+import PF_BLACK from './profile_icon_black.svg';
+import PF_COLOR from './profile_icon_color.svg';
+import WO_BLACK from './workout_icon_black.svg';
+import WO_COLOR from './workout_icon_color.svg';
+
+export const icons = {
+    diet: {
+        black: DI_BLACK,
+        color: DI_COLOR,
+    },
+    profile: {
+        black: PF_BLACK,
+        color: PF_COLOR,
+    },
+    workout: {
+        black: WO_BLACK,
+        color: WO_COLOR,
+    },
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,49 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useRoute } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ProfileScreen from "./screens/profile_screen";
 import WorkoutScreen from "./screens/workout_screen";
 import DietLogScreen from "./screens/dietlog_screen";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { icons } from "../assets/icon/icons";
 
 const Tab = createBottomTabNavigator();
 
-function MainNavigator() {
+function TabNavigator() {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, size }) => {
+                    let IconComponent;
+
+                    if (route.name === "운동") {
+                        IconComponent = focused ? icons.workout.color : icons.workout.black;
+                    } else if (route.name === "식단") {
+                        IconComponent = focused ? icons.diet.color : icons.diet.black;
+                    } else if (route.name === "프로필") {
+                        IconComponent = focused ? icons.profile.color : icons.profile.black;
+                    }
+
+                    if (!IconComponent) {
+                        return null;
+                    }
+
+                    return <IconComponent width={size} height={size} />;
+                },
+                tabBarActiveTintColor: "tomato",
+                tabBarInactiveTintColor: "gray",
+            })}
+        >
             <Tab.Screen name="운동" component={WorkoutScreen} />
             <Tab.Screen name="식단" component={DietLogScreen} />
             <Tab.Screen name="프로필" component={ProfileScreen} />
         </Tab.Navigator>
+    );
+}
+
+function MainNavigator() {
+    return (
+        <TabNavigator />
     );
 }
 
