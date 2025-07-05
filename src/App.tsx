35dbf685085fb3dd4da5b91049564dc5786b4bc6
jungg <jungkwen20@gmail.com>
2025-07-05 11:59:12 +0900

    move folders

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@
  * @format
  */
 
-import { NewAppScreen } from "@react-native/new-app-screen";
-import React from 'react';
+import React from "react";
 import {
     StatusBar,
     StyleSheet,
@@ -19,39 +18,45 @@ import { sendToGemini } from "./services/dietlog_api";
 import "../models/dietlog";
 import { NavigationContainer } from "@react-navigation/native";
 
-const isDarkMode = useColorScheme() === "dark";
-
-const handlePress = async () => {
-    try {
-        const result = await launchImageLibrary({
-            mediaType: "photo",
-            includeBase64: true,
-        });
-
-        if (result.didCancel) {
-            console.log("User cancelled image picker");
-        } else if (result.errorCode) {
-            console.log("ImagePicker Error: ", result.errorMessage);
-        } else if (result.assets && result.assets.length > 0) {
-            const asset = result.assets[0];
-            if (asset.base64 && asset.type) {
-                const response = await sendToGemini(asset.base64, asset.type);
-                console.log("Gemini API Response:", response);
-            }
-        }
-    } catch (error) {
-        console.error(error);
-    }
-};
-
 function App() {
-    
+    const isDarkMode = useColorScheme() === "dark";
 
+    const handlePress = async () => {
+        try {
+            const result = await launchImageLibrary({
+                mediaType: "photo",
+                includeBase64: true,
+            });
+
+            if (result.didCancel) {
+                console.log("User cancelled image picker");
+            } else if (result.errorCode) {
+                console.log("ImagePicker Error: ", result.errorMessage);
+            } else if (result.assets && result.assets.length > 0) {
+                const asset = result.assets[0];
+                if (asset.base64 && asset.type) {
+                    const response = await sendToGemini(
+                        asset.base64,
+                        asset.type
+                    );
+                    console.log("Gemini API Response:", response);
+                }
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    
     return (
         <NavigationContainer>
-			<View style={styles.container}>
-			</View>
-    	</NavigationContainer>
+            <View style={styles.container}>
+                <StatusBar
+                    barStyle={isDarkMode ? "light-content" : "dark-content"}
+                    backgroundColor={isDarkMode ? "#000" : "#fff"}
+                />
+                <Button title="이미지 선택 및 분석" onPress={handlePress} />
+            </View>
+        </NavigationContainer>
     );
 }
 
