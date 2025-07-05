fb510fad9ca3a5f2a6172b0b3b9096a4e8bb3021
jungg <jungkwen20@gmail.com>
2025-07-05 14:06:52 +0900

    default structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from "react";
-import {
-    StatusBar,
-    StyleSheet,
-    useColorScheme,
-    View,
-    Button,
-} from "react-native";
-import { launchImageLibrary } from "react-native-image-picker";
-import { sendToGemini } from "./services/dietlog_api";
-import "../models/dietlog";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ProfileScreen from "./screens/profile_screen";
+import WorkoutScreen from "./screens/workout_screen";
+import DietLogScreen from "./screens/dietlog_screen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-function App() {
-    const isDarkMode = useColorScheme() === "dark";
+const Tab = createBottomTabNavigator();
 
-    const handlePress = async () => {
-        try {
-            const result = await launchImageLibrary({
-                mediaType: "photo",
-                includeBase64: true,
-            });
-
-            if (result.didCancel) {
-                console.log("User cancelled image picker");
-            } else if (result.errorCode) {
-                console.log("ImagePicker Error: ", result.errorMessage);
-            } else if (result.assets && result.assets.length > 0) {
-                const asset = result.assets[0];
-                if (asset.base64 && asset.type) {
-                    const response = await sendToGemini(
-                        asset.base64,
-                        asset.type
-                    );
-                    console.log("Gemini API Response:", response);
-                }
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
-    
+function MainNavigator() {
     return (
-        <NavigationContainer>
-            <View style={styles.container}>
-                <StatusBar
-                    barStyle={isDarkMode ? "light-content" : "dark-content"}
-                    backgroundColor={isDarkMode ? "#000" : "#fff"}
-                />
-                <Button title="이미지 선택 및 분석" onPress={handlePress} />
-            </View>
-        </NavigationContainer>
+        <Tab.Navigator>
+            <Tab.Screen name="운동" component={WorkoutScreen} />
+            <Tab.Screen name="식단" component={DietLogScreen} />
+            <Tab.Screen name="프로필" component={ProfileScreen} />
+        </Tab.Navigator>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-});
+function App() {
+    return (
+        <SafeAreaProvider>
+            <NavigationContainer>
+                <MainNavigator />
+            </NavigationContainer>
+        </SafeAreaProvider>
+    );
+}
 
 export default App;
---
+++ src/models/workout.tsx
@@ -0,0 +1,32 @@
+enum WorkoutCategory {
+    StrengthTraining = "근력 운동",
+    Running = "달리기",
+    Yoga_Pila = "요가/필라테스",
+    Sports = "구기 종목",
+    MartialArts = "무술",
+    Rehabilitation = "재활 운동",
+    Bodyweight = "맨몸 운동",
+    CrossFit = "크로스핏",
+    Swimming = "수영",
+    Other = "기타"
+}
+
+interface IWorkout {
+    workoutId: string;      // use UUID
+    workoutCategory: WorkoutCategory;
+    startTime: Date;
+    endTime: Date;
+    expectedCalory: number;
+    workoutImgUrl: string;
+}
+
+class Workout implements IWorkout {
+    constructor (
+        public workoutId: string,
+        public workoutCategory: WorkoutCategory,
+        public startTime: Date,
+        public endTime: Date,
+        public expectedCalory: number,
+        public workoutImgUrl: string,
+    ) {}
+}
