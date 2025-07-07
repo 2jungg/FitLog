a3624c0378ea0ce030d9818e4a1228384a9c7ea0
boyeonp <dnflwlq20020416@gmail.com>
2025-07-08 00:08:58 +0900

    0708end

--- src/models/profile.tsx
+++ src/models/profile.tsx
@@ -7,6 +7,7 @@ interface IProfile {
     username: string;
     height: number;     // cm unit
     weightLogs: Array<WeightLog>;
+    myGoal: string;
     addWeightLog(_weightlog: WeightLog): void;
     checkWeightLogExists(_weightlog: WeightLog): boolean;
 }
@@ -15,7 +16,8 @@ export class Profile implements IProfile {
     constructor (
         public username: string,
         public height: number,
-        public weightLogs: Array<WeightLog>
+        public weightLogs: Array<WeightLog>,
+        public myGoal: string
     ) {}
     
     addWeightLog(_weightlog: WeightLog): void {
